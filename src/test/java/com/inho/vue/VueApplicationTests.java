b310f508ad532572bf60c46ed6ec29e8a4147675
leeinho <leeinho@soleo.kr>
2021-12-23 14:55:27 +0900

    initial commit

---
+++ src/test/java/com/inho/vue/VueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inho.vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
